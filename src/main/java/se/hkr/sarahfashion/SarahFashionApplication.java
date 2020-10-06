c64ae5e3bff6da58560275e52b76bb3e7996ac19
Amer Harb <amer.harb@gmail.com>
2020-10-06 09:18:12 +0200

    init

---
+++ src/main/java/se/hkr/sarahfashion/SarahFashionApplication.java
@@ -0,0 +1,13 @@
+package se.hkr.sarahfashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarahFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarahFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/hkr/sarahfashion/SarahFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package se.hkr.sarahfashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarahFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
