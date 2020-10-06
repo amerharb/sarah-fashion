ad170157acf0fccc7f01f0eaab8b68c44ded8806
Amer Harb <amer.harb@gmail.com>
2020-10-06 10:20:52 +0200

    add setter and getter to model

--- src/main/java/se/hkr/sarahfashion/server/models/Customer.java
+++ src/main/java/se/hkr/sarahfashion/server/models/Customer.java
@@ -6,11 +6,42 @@ public class Customer {
     private String address;
     private boolean gender; // true for female and false for male TODO: if there is time make this enum
 
+    public Customer(String ssn, String name, String address, boolean gender){
+        this.ssn = ssn;
+        this.name = name;
+        this.address = address;
+        this.gender = gender;
+    }
+
     public void setSsn(String ssn) {
         this.ssn = ssn;
     }
 
     public String getSsn() {
         return ssn;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public boolean isGender() {
+        return gender;
     }
 }
--- src/main/java/se/hkr/sarahfashion/server/repos/CustomerRepository.java
+++ src/main/java/se/hkr/sarahfashion/server/repos/CustomerRepository.java
@@ -15,13 +15,23 @@ public class CustomerRepository {
         Order order1 = new Order();
         Order order2 = new Order();
         // TODO: add order to customer
-        Customer customer1 = new Customer();
+        Customer customer1 = new Customer(
+                "1111",
+                "John Smith",
+                "Smith Family St.",
+                false
+        );
         mockDatabase.customers.add(customer1);
 
         Order order3 = new Order();
         Order order4 = new Order();
         // TODO: add order to customer
-        Customer customer2 = new Customer();
+        Customer customer2 = new Customer(
+                "2222",
+                "Åsa Johansson",
+                "Åsa Mansion",
+                true
+        );
         mockDatabase.customers.add(customer2);
     }
 
